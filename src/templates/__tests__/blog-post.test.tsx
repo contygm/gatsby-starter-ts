652d7ac4bf988cd64080e2491c01ff5069820bf9
contygm <contygm@gmail.com>
2022-11-25 15:30:01 -0800

    update: blog post snap shots

--- src/templates/__tests__/blog-post.test.tsx
+++ src/templates/__tests__/blog-post.test.tsx
@@ -4,6 +4,17 @@ import BlogPost, { Head } from '../../templates/blog-post';
 import { mockBlogPostHeadData, mockBlogPostPageData } from './mock-data';
 
 describe('Blog Posts Page', () => {
+    beforeEach(() => {
+        // IntersectionObserver isn't available in test environment
+        const mockIntersectionObserver = jest.fn();
+        mockIntersectionObserver.mockReturnValue({
+          observe: () => null,
+          unobserve: () => null,
+          disconnect: () => null
+        });
+        window.IntersectionObserver = mockIntersectionObserver;
+    })
+
     it('renders correctly', () => {
         const { asFragment } = render(<BlogPost {...mockBlogPostPageData} />);
         expect(asFragment()).toMatchSnapshot();
