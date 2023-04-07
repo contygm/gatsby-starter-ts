946a66263e2bd892ffce6c75f9d2defc4fcc6750
contygm <contygm@gmail.com>
2023-04-06 21:54:34 -0700

    move legal link constants to util/constants

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,35 +9,7 @@ import {
     faAngleUp
 } from '@fortawesome/free-solid-svg-icons';
 import { FaIconLink } from '../common/FaIconLink';
-
-/**
- * All the legal page links with labels
- * @property {string} label - The display label for the page
- * @property {string} slug - The slug-friendly version of the label. Should be lowercase and replace spaces with `-`
- * 
- * @memberof Footer
- * @see DropDownLinkBtn
- * @constant {Object[]}
- */
-// TODO: move to const folder
-const LEGAL_LINKS = [
-    {
-        label: 'Copyright',
-        slug: 'copyright'
-    },
-    {
-        label: 'Cookie Policy',
-        slug: 'cookie-policy'
-    },
-    {
-        label: 'Terms and Conditions',
-        slug: 'terms-and-conditions'
-    },
-    {
-        label: 'Disclaimer',
-        slug: 'disclaimer'
-    }
-];
+import { LEGAL_LINKS } from '../../utils/constants/legalDropdownLinks';
 
 /**
  * A text link with footer styles
---
+++ src/utils/constants/legalDropdownLinks.ts
@@ -0,0 +1,27 @@
+/**
+ * All the legal page links with labels
+ * @property {string} label - The display label for the page
+ * @property {string} slug - The slug-friendly version of the label. Should be lowercase and replace spaces with `-`
+ * 
+ * @memberof Footer
+ * @see DropDownLinkBtn
+ * @constant {Object[]}
+ */
+export const LEGAL_LINKS = [
+    {
+        label: 'Copyright',
+        slug: 'copyright'
+    },
+    {
+        label: 'Cookie Policy',
+        slug: 'cookie-policy'
+    },
+    {
+        label: 'Terms and Conditions',
+        slug: 'terms-and-conditions'
+    },
+    {
+        label: 'Disclaimer',
+        slug: 'disclaimer'
+    }
+];
